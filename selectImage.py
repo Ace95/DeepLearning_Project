9e87e2844a112e6314ddd2a31afb858461991583
Nicolaas Ruberg <nick.ruberg@gmail.com>
2020-04-14 16:38:05 +0200

    More changes

--- selectImage.py
+++ selectImage.py
@@ -11,8 +11,11 @@ import shutil
 import cv2
 
 destDIR='./orient/train'
+#destDIR='./orient/test'
 origDIR='./img_align_celeba'
-
+totalFiles=200
+startFiles = 0
+endFiles = startFiles + totalFiles
 
 #Store in list the name of the files
 
@@ -38,8 +41,6 @@ def getListOfFiles(dirName):
 listOfFiles = getListOfFiles(origDIR)
 listOfFiles.sort()
 
-startFiles = 0
-endFiles = 20
 
 for filename in listOfFiles[startFiles:endFiles]:
     if filename.endswith(('.jpeg','.jpg')):
@@ -49,11 +50,11 @@ for filename in listOfFiles[startFiles:endFiles]:
             cv2.imshow('Celeba Image',imgCeleba)
             keyPressed = cv2.waitKey(0)
             if keyPressed == ord ('a') or keyPressed == 2 : # a key or left arrow
-                shutil.move(filename,destDIR+'/left/'+path.basename(filename))
+                shutil.move(filename,destDIR+'/left_pose/'+path.basename(filename))
             elif keyPressed == ord ('k')  or keyPressed == 3 :
-                shutil.move(filename,destDIR+'/right/'+path.basename(filename))
+                shutil.move(filename,destDIR+'/right_pose/'+path.basename(filename))
             elif keyPressed == 32 or keyPressed == 0 or keyPressed == 1: # Space bar
-                shutil.move(filename,destDIR+'/center/'+path.basename(filename))
+                shutil.move(filename,destDIR+'/center_pose/'+path.basename(filename))
             else:
                 print('{} : Invalid key pressed. Please, a for left, k for right and spacebar for center.'.format(keyPressed))
                       
